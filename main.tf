e44f5bcd32d325c483f8d8532430c851bdee142e
Chimtamreddy <kr7348202@gmail.com>
2024-04-18 16:08:42 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -17,6 +17,12 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_route" "igw" {
+  route_table_id            = module.subnets["public"]["route_table_ids"]
+  destination_cidr_block    = "10.0.1.0/22"
+  vpc_peering_connection_id = "pcx-45ff3dc1"
+}
+
 output "subnet" {
   value = module.subnets
 }
