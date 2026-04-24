b36c80239e46614f153c28fea46cc7b745d16f0b
chimtamreddy <7348202@gmail.com>
2026-04-24 16:28:33 +0530

    Creating Output

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_route" "igw" {
-  for_each = lookup(lookup(var.subnets, "public", null), "route_table", null)
-  route_table_id            = each.key[id]
+  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id            = each.value["id"]
   destination_cidr_block    = "0.0.0.0/0"
 }
 
